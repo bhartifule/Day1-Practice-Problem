b19e54ef83f792daf74bc04bf8fa722fd742ed58
Bharti <bharatifule10gmail.com>
2023-01-10 13:57:02 +0530

    Added:Day1 Practice Problem

---
+++ bridgelabz/day1/CommandLine.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.day1;
+
+public class CommandLine {
+	public static void main(String[] args) {
+		String arg1 = args[0];
+		System.out.println(arg1);
+	}
+}
---
+++ bridgelabz/day1/DisplayMassage.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.day1;
+
+public class DisplayMassage {
+	public static void main(String[] args) {
+		System.out.println("Hi I am Bharti Fule");
+	}
+
+}
